201c25e4497c1a62298920718d1d1a620d2f34cd
Daniel Hoesle <dhoesle21@gmail.com>
2020-07-16 08:53:19 -0600

    completed styling

--- app/src/components/Quote.js
+++ app/src/components/Quote.js
@@ -13,11 +13,17 @@ const Quote = ({ getQuote, quote, isFetching, error }) => {
     }
 
     return (
-        <div className='Quote'>
-            <h3>"{quote}"<br></br>-Kanye West</h3>
+        <>
+            <h1>The Kanye West Quote Generator</h1>
 
-            <button onClick={getQuote}>One more maybe?</button>        
-        </div>
+            <div className='Button'>
+                <button onClick={getQuote}>Yes! More Wisdom!</button>        
+            </div>
+            <div className='Quote'>
+                <h3>"{quote}"<br></br>-Kanye West</h3>
+            </div>
+        </>
+
     )
 }
 
